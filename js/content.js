33c43ddeb8b310fe6d12d164867b445f8361bba3
Munus Shih <munusshih@newschool.edu>
2024-02-23 00:06:30 -0500

    week 5

--- js/content.js
+++ js/content.js
@@ -17,6 +17,7 @@ fetchAndInsertContent("week1", "../../weeks/1.html");
 fetchAndInsertContent("week2", "../../weeks/2.html");
 fetchAndInsertContent("week3", "../../weeks/3.html");
 fetchAndInsertContent("week4", "../../weeks/4.html");
+fetchAndInsertContent("week5", "../../weeks/5.html");
 // fetchAndInsertContent("accessible-book", "../../assignments/accessible-book.html");
 // fetchAndInsertContent("experimental-clock", "../../assignments/experimental-clock.html");
 // fetchAndInsertContent("generative-tool", "../../assignments/generative-tool.html");
