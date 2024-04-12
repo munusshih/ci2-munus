209f439bf18fd9f1923d98e8a6961a46dc955e5f
Munus Shih <munusshih@newschool.edu>
2024-04-12 16:27:39 -0400

    update week 11

--- js/params.js
+++ js/params.js
@@ -86,4 +86,23 @@ function stopStepInput() {
     clearInterval(intervalId);
 }
 
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 
+    const hourHand = document.querySelector('.hour-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const secondHand = document.querySelector('.second-hand');
+
+    const hourDegrees = (hours % 12) * 30 + minutes / 2;
+    const minuteDegrees = minutes * 6 + seconds / 10;
+    const secondDegrees = seconds * 6;
+
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+setInterval(updateClock, 1000);
